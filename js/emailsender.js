72946703adde30918fe6b04f3e19571f7cfca076
Sherlock67 <alammushfiqul366@gmail.com>
2025-03-23 01:49:23 +0600

    email js integrating

---
+++ js/emailsender.js
@@ -0,0 +1,19 @@
+(function() {
+    emailjs.init({
+      publicKey: "FfDG2wmIWrZuFjhQx",
+    });
+})();
+
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_5sk8orj', 'template_gwx4722', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
