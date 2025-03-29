981eb7dc08953bc8dc33e3a1dc224a02a76824a2
Sherlock67 <alammushfiqul366@gmail.com>
2025-03-29 16:48:52 +0600

    about me section

--- js/emailsender.js
+++ js/emailsender.js
@@ -9,7 +9,7 @@ window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         // these IDs from the previous steps
-        emailjs.sendForm('service_5sk8orj', 'template_gje4dn8', this)
+        emailjs.sendForm('service_5sk8orj', 'template_2ccvei7', this)
             .then(() => {
                 console.log('SUCCESS!');
             }, (error) => {
